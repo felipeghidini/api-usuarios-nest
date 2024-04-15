bb29cac194f4346c60296ebe9508dbc4fd40e53d
felipeghidini <lipeghidini18@gmail.com>
2024-04-15 16:29:34 -0300

    feat: projeto criado e adicionado post e get

---
+++ src/usuario/controllers/usuario.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { UsuarioRepository } from "src/usuario/repositories/usuario.repository";
+
+@Controller('/usuarios')
+export class UsuarioController {
+
+  constructor(private usuarioRepository: UsuarioRepository) {}
+
+  @Post()
+  async criaUsuario(@Body() dadosDoUsuario) {
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    return dadosDoUsuario;
+  }
+
+  @Get()
+  async listarUsuarios() {
+    return this.usuarioRepository.listar();
+  }
+}
